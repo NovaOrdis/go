96e177f80730818a88a11900d7fdef1f03b902e9
Ovidiu Feodorov <ovidiu@novaordis.com>
2016-04-04 04:04:48 -0700

    build logic work

--- commands/bakm/src/config.go
+++ commands/bakm/src/config.go
@@ -11,15 +11,15 @@ type config struct {
 }
 
 // initializes a blank config instance from command line arguments
-func (config config) init(args []string) error {
+func (config *config) init(args []string) error {
+
+    var err error
 
     if len(args) < 2 {
         return errors.New("no target directory specified")
     }
 
     config.dirName = args[1]
-
-    var err error
 
     config.dirPtr, err = os.Open(config.dirName)
 
