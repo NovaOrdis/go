7d3358185d7d87d27a9fd501e95aa752b00c966a
Ovidiu Feodorov <ovidiu@novaordis.com>
2016-04-04 02:05:20 -0700

    build tools

--- commands/bakm/bin/go-build-tools.sh
+++ commands/bakm/bin/go-build-tools.sh
@@ -2,18 +2,63 @@
 
 function main() {
 
+    local project_home=$(dirname $0)/..
     local command=$(basename $0)
-    ${command}
+    ${command} ${project_home}
 }
 
 function build() {
-    echo "building ..."
+
+    local project_home=$1
+    local executable_name=$(get-executable-name ${project_home})
+
+    echo -n "building ${executable_name} ... "
+    local output_dir=${project_home}/output
+    [ ! -d ${output_dir} ] && mkdir ${project_home}/output
+
+    go build -o ${output_dir}/${executable_name} ${project_home}/src/*.go
+    echo "done"
 }
 
 function clean() {
-    echo "cleaning ..."
+
+    local project_home=$1
+
+    echo -n "cleaning ... "
+
+    local output_dir=${project_home}/output
+    [ -d ${output_dir} ] && rm -r ${output_dir}
+    echo "done"
 }
 
+#
+# returns 1 and empty output if cannot figure it out
+#
+function get-executable-name() {
+
+    local project_home=$1
+
+    #
+    # look for the .go file that contains a "main" package and a main() function and use that name
+    #
+
+    for i in $(ls ${project_home}/src/*.go); do
+
+        if ! grep -q "^package main$" ${i}; then
+            continue
+        fi
+
+        if ! grep -q "^func main() *{$" ${i}; then
+            continue
+        fi
+
+        echo $(basename ${i} .go)
+        return 0
+    done
+
+    # not found
+    return 1
+}
 
 main $@
 
